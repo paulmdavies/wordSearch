448f33558049927dff8bf87906541493535bc4c5
Paul Davies <paulmdavies@gmail.com>
2020-12-14 09:02:26 +0000

    Increase grid size to 7

--- static/start.js
+++ static/start.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
       });
     })
 
-  const DIMENSION = 5;
+  const DIMENSION = 7;
 
   const DISTRIBUTION = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1]
   const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
